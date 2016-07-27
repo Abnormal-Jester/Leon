9d9865d6d89a2288110754c927b112e355e79d69
Jarett Lee <Abnormal-Jester@users.noreply.github.com>
2016-07-27 14:03:30 -0400

    Revert "Add narginchk"
    This reverts commit 1702b3bb2302a3f9e89523ca8a8120225e95a527.

--- version_functions/narginchk.m
+++ version_functions/narginchk.m
@@ -1,29 +1,11 @@
-function narginchk(minargs, maxargs)
 %NARGINCHK Validate number of input arguments. 
 %   NARGINCHK(LOW,HIGH) throws an error if nargin is less than LOW or
 %   greater than HIGH.
 %
 %   Example
 %      narginchk(1, 3)
+%
+%   See also NARGOUTCHK, NARGIN, INPUTNAME, ERROR.
 
-  if (nargin ~= 2)
-    error('%s: Usage: narginchk(minargs, maxargs)',upper(mfilename));
-  elseif (~isnumeric (minargs) || ~isscalar (minargs))
-    error ('minargs must be a numeric scalar');
-  elseif (~isnumeric (maxargs) || ~isscalar (maxargs))
-    error ('maxargs must be a numeric scalar');
-  elseif (minargs > maxargs)
-    error ('minargs cannot be larger than maxargs')
-  end
-
-
-  args = evalin ('caller', 'nargin;');
-
-
-  if (args < minargs)
-    error ('not enough input arguments');
-  elseif (args > maxargs)
-    error ('too many input arguments');
-  end
-
-end
+%   Copyright 2010 The MathWorks, Inc.
+%   Built-in function.
