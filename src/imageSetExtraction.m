1b19e27bd17f2e95f6708f8da91ad2070396d3a2
Jarett Lee <Abnormal-Jester@users.noreply.github.com>
2016-07-28 15:04:12 -0400

    Fix image set extraction error handling

--- src/imageSetExtraction.m
+++ src/imageSetExtraction.m
@@ -2,10 +2,10 @@ try
     imageSetNotice;
     [colorImages, irImages, colorDir, irDir] = imageSetSelection;
     [autoImagePoints, boardSize, checkerboardPairsUsed] = imageSetProcessing (colorImages, irImages);
-catch myError
+catch
     clearvars colorImages irImages colorDir irDir autoImagePoints boardSize checkerboardPairsUsed;
+    fprintf (1, 'Failed.\n\n')
     reopenGUI;
-    rethrow myError;
 end
 
 reopenGUI;
