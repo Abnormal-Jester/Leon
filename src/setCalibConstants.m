7e3f3882c0d761519f9a87024eea3e1af5cb743d
Jarett Lee <Abnormal-Jester@users.noreply.github.com>
2016-07-28 13:54:03 -0400

    Refactor setConstants into setCalibConstants
    Add comments and clear unused variables.

--- src/setCalibConstants.m
+++ src/setCalibConstants.m
@@ -1,19 +1,24 @@
 
 fprintf(1, 'Obtaining constants...\n');
 
-defaultSquareSize = 73;
-
 % Refocuses the window onto the Command Window for the input prompt
 commandwindow;
 
+defaultSquareSize = 73;
 squareSize = input(['Size of a side of each square ([]=' num2str(defaultSquareSize) 'mm) = ']);
 if isempty(squareSize), squareSize = defaultSquareSize; end;
 
 dX = squareSize;
 dY = squareSize;
 
+clearvars squareSize defaultSquareSize;
+
+
 fprintf(1, '\n');
 
+% The calibration number is usually included in the directory path, so I print
+% the directory paths here. It also allows the user to check if the directory
+% paths are correct.
 fprintf(1, 'Color dir: ');
 disp(colorDir);
 fprintf(1, 'IR dir: ');
@@ -21,8 +26,10 @@ disp(irDir);
 
 cameraID = input('Camera ID number = ');
 
+
 fprintf(1, 'Constants obtained.\n\n');
 
+% Opens the GUI component if it exists.
 if ishandle(1),
     figure(1);
 end
