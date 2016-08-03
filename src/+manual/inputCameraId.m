3e283f28d37456aa766cc7a07dc65fd1e086adda
Jarett Lee <Abnormal-Jester@users.noreply.github.com>
2016-08-03 16:37:06 -0400

    Improve camera input function

--- src/+manual/inputCameraId.m
+++ src/+manual/inputCameraId.m
@@ -1,6 +1,20 @@
-function cameraId = inputCameraId
-%INPUTCAMERAID Prompts the user for the id of the camera.
+function cameraId = inputCameraId(defaultId)
+%inputCameraId Prompts the user for the id of the camera.
+%   cameraId = inputCameraId
+%   cameraId = inputCameraId(defaultId)
 
-cameraId = input('Camera ID number = ');
+if nargin == 0
+    defaultId = 0;
+else
+
+try
+    cameraId = input(['Camera ID number (' num2str(defaultId) ') = ']);
+
+    if isblank(cameraId),
+        cameraId = defaultId;
+    end
+catch
+    cameraId = defaultId;
+end
 
 end
