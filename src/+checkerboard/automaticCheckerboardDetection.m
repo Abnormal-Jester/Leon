d5a0378753553bd8604dc22e93d5c026c6f31258
Jarett Lee <Abnormal-Jester@users.noreply.github.com>
2016-08-08 11:30:16 -0400

    Alter to fit style guide

--- src/+checkerboard/automaticCheckerboardDetection.m
+++ src/+checkerboard/automaticCheckerboardDetection.m
@@ -1,21 +1,24 @@
-function [colorData, irData, boardData] = automaticCheckerboardDetection (colorImageSet, irImageSet)
-% AUTOMATICCHECKERBOARDDETECTION Automatically detect the checkerboards
-%   [COLORDATA, IRDATA, BOARDDATA] = AUTOMATICCHECKERBOARDDETECTION (COLORIMAGESET, IRIMAGESET)
+function [colorData, irData, boardData] = automaticCheckerboardDetection(colorImageSet, irImageSet)
+% automaticCheckerboardDetection Automatically detect the checkerboards
+%   [colorData, irData, boardData] = automaticCheckerboardDetection(colorImageSet, irImageSet)
 %   uses the MATLAB Computer Vision System Toolbox to automatically detect some
 %   of the checkerpoints and constructs the data objects the calibration program
 %   requires.
 
 import checkerboard.*;
 
+% MATLAB Computer Vision System Toolbox
 [autoImagePoints, boardSize, imagePairsUsed] =...
     detectCheckerboardPoints(colorImageSet.ImageLocation, irImageSet.ImageLocation);
 
+% Create the image set data objects
 tempPoints = autoImagePoints(:,:,:,1);
 colorData = ImageSetData(tempPoints, colorImageSet, imagePairsUsed);
 
 tempPoints = autoImagePoints(:,:,:,2);
 irData = ImageSetData(tempPoints, irImageSet, imagePairsUsed);
 
+% Create the board data object
 boardData = CheckerboardData(boardSize, imagePairsUsed);
 
 end
